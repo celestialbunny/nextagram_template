ded57184d1e10a3e8d5c729500c1c7e0c8a1e0a2
celestialbunny <coronateh@outlook.com>
2019-02-21 08:18:01 +0800

    register page built

--- app.py
+++ app.py
@@ -1,25 +1,49 @@
 import os
 import config
-from flask import Flask
+from flask import (Flask, g, render_template, flash, redirect, url_for)
+from flask_bcrypt import check_password_hash
+from flask_login import (LoginManager, login_user, current_user, logout_user, login_required)
+
 from models.base_model import db
+from models.user import User
+from models.forms import RegistrationForm, LoginForm
 
 web_dir = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), 'instagram_web')
+	os.path.abspath(__file__)), 'instagram_web')
 
 app = Flask('NEXTAGRAM', root_path=web_dir)
 
 if os.getenv('FLASK_ENV') == 'production':
-    app.config.from_object("config.ProductionConfig")
+	app.config.from_object("config.ProductionConfig")
 else:
-    app.config.from_object("config.DevelopmentConfig")
+	app.config.from_object("config.DevelopmentConfig")
 
 
 @app.before_request
 def before_request():
-    db.connect()
+	db.connect()
 
 
 @app.after_request
 def after_request(response):
-    db.close()
-    return response
+	db.close()
+	return response
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	# if form.validate_on_submit():
+	if form.validate():
+		flash("Thanks for registering", "success")
+		User.create_user(
+			username=form.username.data,
+			email=form.email.data,
+			password=form.password.data
+		)
+		return redirect(url_for('index'))
+	return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():
+	pass
--- models/user.py
+++ models/user.py
@@ -1,6 +1,22 @@
 from models.base_model import BaseModel
+from flask_bcrypt import generate_password_hash
 import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False, null=False)
+	username = pw.CharField(unique=True, null=False)
+	email = pw.CharField(unique=True, null=False)
+	password = pw.CharField(unique=True, null=False)
+
+	@classmethod
+	def create_user(cls, username, email, password, admin=False):
+		try:
+			cls.create(
+				username=username,
+				email=email,
+				password=generate_password_hash(password),
+				is_admin=admin
+			)
+		# Integrity error will be thrown if the username and email are NOT actually unique (duplicated in other words)
+		except IntegrityError:
+			raise ValueError("Either username and/or email has been taken")
